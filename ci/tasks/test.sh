d706692b0510be632ee9a8cee405fe4a50d0be00
Divya Dadlani <divya.dadlani@gmail.com>
2018-11-21 17:13:23 -0500

    curl kubectl

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-apt install kubectl
+apt install curl
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 
