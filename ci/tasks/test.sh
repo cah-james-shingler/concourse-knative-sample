50573a5c345dd0f4e522e50c739fd297de38ad46
Divya Dadlani <divya.dadlani@gmail.com>
2018-11-23 15:28:52 -0500

    correct args

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -2,7 +2,8 @@
 
 
 echo $CONFIG > key.json
-gcloud auth activate-service-account kubectl --key-file=key.json
+
+gcloud auth activate-service-account --key-file=key.json
 
 gcloud container clusters knative-test --zone=us-east1-b
 
