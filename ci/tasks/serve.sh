6e33ca7dc1122f7c6d1dfc669939f46b7d950d1a
Dale Wick <dwick@pivotal.io>
2018-11-28 18:03:42 -0500

    Added serve node to the pipeline.

---
+++ ci/tasks/serve.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo $CONFIG > key.json
+
+gcloud auth activate-service-account --key-file=key.json
+
+gcloud config set project $PROJECT_NAME
+
+gcloud container clusters get-credentials $CLUSTER_NAME --zone=$ZONE
+
+# Build the container on your local machine
+docker build -t ((username))/helloworld-go .
+
+# Push the container to docker registry
+docker push ((username))/helloworld-go
+
+kubectl version
+
+kubectl apply -f knative-app/serve.yaml
+
