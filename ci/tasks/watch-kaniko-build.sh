1d2d0eb30fd76306ee334f532756a6acb0ea6b3f
Divya Dadlani <ddadlani@pivotal.io>
2018-11-29 17:48:24 -0500

    kubectl logs, not get logs

--- ci/tasks/watch-kaniko-build.sh
+++ ci/tasks/watch-kaniko-build.sh
@@ -4,9 +4,9 @@
 
 setup_kubectl
 
-kubectl get logs -l build.knative.dev/buildName=kaniko-build -c build-step-credential-initializer
-kubectl get logs -l build.knative.dev/buildName=kaniko-build -c build-step-git-source
-kubectl get logs -l build.knative.dev/buildName=kaniko-build -c build-step-build-and-push
+kubectl logs -l build.knative.dev/buildName=kaniko-build -c build-step-credential-initializer
+kubectl logs -l build.knative.dev/buildName=kaniko-build -c build-step-git-source
+kubectl logs -l build.knative.dev/buildName=kaniko-build -c build-step-build-and-push
 
 kubectl get build kaniko-build -ojson
 
