9ced7fc151f08163024ba5e4cef9f8c7a27eab4e
Divya Dadlani <ddadlani@pivotal.io>
2018-11-28 21:42:23 -0500

    Remove 'knative-app' from command

--- ci/tasks/build-kaniko-image.sh
+++ ci/tasks/build-kaniko-image.sh
@@ -3,11 +3,11 @@
 echo "Setting up kubectl to point to cluster '$CLUSTER_NAME'"
 echo $CONFIG > key.json
 
-gcloud auth activate-service-account --key-file=key.json
+gcloud auth activate-service-account --key-file=key.json --quiet
 
 gcloud config set project $PROJECT_NAME
 
 gcloud container clusters get-credentials $CLUSTER_NAME --zone=$ZONE
 
 echo "Creating new build"
-kubectl apply -f knative-app/build-kaniko.yaml
+kubectl apply -f knative-test/build-kaniko.yaml
