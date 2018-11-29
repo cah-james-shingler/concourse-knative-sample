621faa44c48453941ba1cee5cf35c3e5ff255e00
Dale Wick <dwick@pivotal.io>
2018-11-29 18:21:01 -0500

    Fill in config for serve task

--- ci/tasks/serve.sh
+++ ci/tasks/serve.sh
@@ -2,19 +2,14 @@
 
 echo $CONFIG > key.json
 
-gcloud auth activate-service-account --key-file=key.json
+pushd helloworld-go
+  # Build the container on your local machine
+  docker build -t ddadlani/helloworld-go .
 
-gcloud config set project $PROJECT_NAME
-
-gcloud container clusters get-credentials $CLUSTER_NAME --zone=$ZONE
-
-# Build the container on your local machine
-docker build -t ((username))/helloworld-go .
-
-# Push the container to docker registry
-docker push ((username))/helloworld-go
+  # Push the container to docker registry
+  docker push ddadlani/helloworld-go
+popd
 
 kubectl version
 
 kubectl apply -f knative-test/serve.yaml
-
