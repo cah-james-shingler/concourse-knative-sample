ea9b223532596c97ef2cd93292e282949752f472
Divya Dadlani <ddadlani@pivotal.io>
2018-11-29 18:24:39 -0500

    fix log command again

--- ci/tasks/watch-kaniko-build.sh
+++ ci/tasks/watch-kaniko-build.sh
@@ -6,9 +6,9 @@ setup_kubectl
 
 PODNAME=kubectl get build kaniko-build -ojsonpath='{.status.cluster.podName}'
 
-kubectl logs -f $PODNAME -c build-step-credential-initializer
-kubectl logs -f $PODNAME -c build-step-git-source
-kubectl logs -f $PODNAME -c build-step-build-and-push
+kubectl logs -f -p $PODNAME -c build-step-credential-initializer
+kubectl logs -f -p $PODNAME -c build-step-git-source
+kubectl logs -f -p $PODNAME -c build-step-build-and-push
 
 status=$(kubectl get po -l build.knative.dev/buildName=kaniko-build -o=jsonpath='{..status.phase}')
 
