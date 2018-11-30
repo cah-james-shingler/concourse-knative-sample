50ce31a633b3a166b4d559c6b512c3e3a04ef048
Divya Dadlani <ddadlani@pivotal.io>
2018-11-30 18:06:51 -0500

    remove docker build from serve.sh

--- ci/tasks/serve.sh
+++ ci/tasks/serve.sh
@@ -7,14 +7,6 @@ gcloud auth activate-service-account --key-file=key.json --quiet
 gcloud config set project $PROJECT_NAME
 gcloud container clusters get-credentials $CLUSTER_NAME --zone=$ZONE
 
-pushd helloworld-go
-  # Build the container on your local machine
-  docker build -t ddadlani/helloworld-go .
-
-  # Push the container to docker registry
-  docker push ddadlani/helloworld-go
-popd
-
 kubectl version
 
 kubectl apply -f knative-test/service.yaml
