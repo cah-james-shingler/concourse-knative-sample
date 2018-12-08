ff636e8842863985ee3830a22bf4266aaef40d8b
Divya Dadlani <ddadlani@pivotal.io>
2018-12-08 00:08:07 -0500

    remove unused comments from serve.sh

--- ci/tasks/serve.sh
+++ ci/tasks/serve.sh
@@ -5,11 +5,7 @@ set +x
 . knative-deployment/ci/tasks/setup.sh
 
 setup_kubectl
-# echo $CONFIG > key.json
-# gcloud auth activate-service-account --key-file=key.json --quiet
-# gcloud config set project $PROJECT_NAME
-# gcloud container clusters get-credentials $CLUSTER_NAME --zone=$ZONE
 
-# kubectl version
+sleep 10
 
 kubectl apply -f knative-deployment/service.yaml
--- set-pipeline.sh
+++ set-pipeline.sh
@@ -6,6 +6,4 @@ fly -t sample sp -p knative -c ci/pipeline.yml \
   -v "key=$(lpass show --notes 'Cluster Key')" \
   -v "cluster=knative-test" \
   -v "project=lively-sentry-221201" \
-  -v "zone=us-east1-b" \
-  -v "docker-username=$(lpass show --username Docker)" \
-  -v "docker-password=$(lpass show --password Docker)"
+  -v "zone=us-east1-b"
